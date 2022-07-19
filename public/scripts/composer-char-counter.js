6727285d4422d997ff9850158c9fa58648dfa96c
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-07-19 19:49:46 +0000

    added tweet counter, styled tweet input

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  // const element = document.getElementById("tweet-text");
+  // element.addEventListener("keypress", (event) => {
+  //   console.log('keypress: ', event.key);
+  // })
+
+  $("#tweet-text").on("input", function() {
+
+    let charCount = 140 - $(this).val().length;
+
+    let counterRef = $(this).parents("form").find("output")
+
+    $(counterRef).html(charCount)
+
+    charCount < 0 ? 
+    $(counterRef).addClass("counterRed") : 
+    $(counterRef).removeClass("counterRed")
+
+  });
+
+});
